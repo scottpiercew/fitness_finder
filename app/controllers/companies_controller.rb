7a0911f855164e22287209043debaba7fae7b99c
Scott Pierce <scottpiercew@gmail.com>
2017-06-13 10:35:29 -0400

    CRUD complete for companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,25 +1,40 @@
 class CompaniesController < ApplicationController
   def index
-    
+    @company = Company.all
+    @event = Event.all
   end
 
+  def show
+    @company = Company.find(params[:id])
+  end
 
   def new
+    @company = Company.new
   end
 
   def create
-  end
-
-  def update
+    @company = Company.create!(company_params)
+    redirect_to company_path(@company)
   end
 
   def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    @company.update(company_params)
+    redirect_to company_path(@company)
   end
 
   def destroy
+    @company = Company.find(params[:id])
+    @company.destroy
+    redirect_to companies_path
   end
 
-
-  def show
+  private
+  def company_params
+    params.require(:company).permit(:name, :company_type, :img_url)
   end
 end
